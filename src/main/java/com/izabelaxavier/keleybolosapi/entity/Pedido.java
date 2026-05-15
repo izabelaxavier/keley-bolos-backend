7d6e5429c52e9f0eac2d82a197e799d21b15366c
Izabela Xavier <izabelaxavier89@gmail.com>
2026-05-15 14:55:42 -0300

    feat: criação da entidade Pedido

---
+++ src/main/java/com/izabelaxavier/keleybolosapi/entity/Pedido.java
@@ -0,0 +1,22 @@
+package com.izabelaxavier.keleybolosapi.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import jakarta.persistence.*;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Entity
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantidade;
+    private String formaPagamento;
+    private String observacoes;
+    private LocalDate dataRetirada;
+    private LocalTime horarioRetirada;
+}
